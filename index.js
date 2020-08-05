e00a3c33fca08df7f3e3c25130e5eedda93faf76
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-05 00:45:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function exerciseByronThePoodle() {
 
 function exerciseDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
-  console.log(`Leash ${dogName}the poodl`);
-  console.log(`Walk to the park with ${dogName} the poodle`);
-  console.log(`hrow the frisbee for ${dogName} the poodle`);
-  console.log(`Walk home with ${dogName} the poodle`);
-  console.log(`Unleash ${dogName} the poodle`);
+  console.log(`Leash ${dogName}the ${dogBreed}`);
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  console.log(`hrow the frisbee for ${dogName} the ${dogBreed}`);
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
 }

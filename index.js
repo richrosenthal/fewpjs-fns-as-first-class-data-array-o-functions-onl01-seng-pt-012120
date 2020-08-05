9c202ca2e200b625b1949dc487b8a02c606da990
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-05 00:43:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function exerciseByronThePoodle() {
-  
+  console.log("Wake Byron the poodle");
+  console.log("Leash Byron the poodle");
+  console.log("Walk to the park with Byron the poodle");
+  console.log("Throw the frisbee for Byron the poodle");
+  console.log("Walk home with Byron the poodle");
+  console.log("Unleash Byron the poodle");
+}
+
+function exerciseDog(dogName, dogBreed) {
+  console.log("Wake Byron the poodle");
+  console.log("Leash Byron the poodle");
+  console.log("Walk to the park with Byron the poodle");
+  console.log("Throw the frisbee for Byron the poodle");
+  console.log("Walk home with Byron the poodle");
+  console.log("Unleash Byron the poodle");
 }

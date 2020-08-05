df50427f8dd3b046b36daa78a659e06d00070333
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-05 00:54:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,4 +34,4 @@ function unleashDog(dogName, dogBreed){
   return string;
 }
 
-let routine = [wakeDog(), leashDog(), walkToPark(), throwFrisbee(), walkHome(), unleashDog()];
+let routine = [wakeDog(dogName, dogBreed), leashDog(dogName, dogBreed), walkToPark(dogName, dogBreed), throwFrisbee(dogName, dogBreed), walkHome(dogName, dogBreed), unleashDog(dogName, dogBreed)];

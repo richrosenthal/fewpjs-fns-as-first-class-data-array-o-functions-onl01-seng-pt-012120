cf418f22fecb8ccadebb7b924dc5b2a785f9d670
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-05 00:51:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,26 +5,31 @@ function wakeDog(dogName, dogBreed){
 }
 
 function leashDog(dogName, dogBreed){
+  let string = `Leash ${dogName}the ${dogBreed}`
   console.log(string);
   return string;
 }
 
 function walkToPark(dogName, dogBreed){
+ let string = `Walk to the park with ${dogName} the ${dogBreed}`;
  console.log(string);
   return string;
 }
 
 function throwFrisbee(dogName, dogBreed){
+  let string = `Throw the frisbee for ${dogName} the ${dogBreed}`;
   console.log(string);
   return string;
 }
 
 function walkHome(dogName, dogBreed){
+  let string = `Walk home with ${dogName} the ${dogBreed}`
   console.log(string);
   return string;
 }
 
 function unleashDog(dogName, dogBreed){
+  let string = `Unleash ${dogName} the ${dogBreed}`
   console.log(string);
   return string;
 }
